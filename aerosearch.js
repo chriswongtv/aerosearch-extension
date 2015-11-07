f72d6db6217411b4a6994b690a0eadb4fbec8b49
Chris Wong <chris@cogifire.com>
2015-11-07 14:37:07 -0800

    v1.0

---
+++ aerosearch.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// The onClicked callback function.
+function onClickHandler(info, tab) {
+  console.log(info.srcUrl);
+  chrome.tabs.create({ url: "http://www.google.com" });
+};
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+// Set up context menu tree at install time.
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({"title": "AeroSearch!", "contexts": ["image"], "id": "parent"});
+});
